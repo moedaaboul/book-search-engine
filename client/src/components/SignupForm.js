848a23f239d2833b882ef49fee25b81fd225645c
Muhammad Daaboul <muhammad.a.daaboul@gmail.com>
2022-04-08 11:11:48 +0100

    removes error msg rendering on Signup form successful submit

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -47,7 +47,7 @@ const SignupForm = () => {
 
       // const { token, user } = await response.json();
       console.log(data);
-      Auth.login(data.login.token);
+      Auth.login(data.addUser.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
