e40236136ba324e61872da76d40c51f264632442
Muhammad Daaboul <muhammad.a.daaboul@gmail.com>
2022-04-15 00:16:29 +0100

    fixed link property and added JWT SECRET to env

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -47,14 +47,14 @@ const SearchBooks = () => {
       }
 
       const { items } = await response.json();
-
+      console.log(items);
       const bookData = items.map((book) => ({
         bookId: book.id,
         authors: book.volumeInfo.authors || ['No author to display'],
         title: book.volumeInfo.title,
         description: book.volumeInfo.description,
         image: book.volumeInfo.imageLinks?.thumbnail || '',
-        link: book.volumeInfo.selfLink,
+        link: book.volumeInfo.infoLink,
       }));
 
       setSearchedBooks(bookData);
